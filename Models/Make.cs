90e0a893546bc6eab1c5c5d42fd1527e164ab683
ashan <madu2000official@gmai.com>
2018-01-13 15:24:01 +0530

    new model classes added.

---
+++ Models/Make.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace VEGA.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public ICollection<Model> Models { get; set; }
+
+        public Make()
+        { 
+            Models = new Collection<Model>();
+        }
+    }
+}
---
+++ Models/Model.cs
@@ -0,0 +1,13 @@
+namespace VEGA.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public Make Make { get; set; }
+
+        public int MakeId { get; set; }
+    }       
+}
