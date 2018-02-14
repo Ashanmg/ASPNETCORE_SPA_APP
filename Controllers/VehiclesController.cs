7b1e3c64f6b890d22b47869cec457316a67c4faf
ashan <madu2000official@gmai.com>
2018-02-14 22:06:00 +0530

    controller modified

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,11 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using VEGA.Models;
+
 namespace VEGA.Controllers
 {
+    [Route("api/vehicles")]
     public class VehiclesController : Controller
     {
         [HttpPost]
         public IActionResult CreateVehicle(Vehicle vehicle)
         {
-
+            return Ok(vehicle);
         }
     }
 }
